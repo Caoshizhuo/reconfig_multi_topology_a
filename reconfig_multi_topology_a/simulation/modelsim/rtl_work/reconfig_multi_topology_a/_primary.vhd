383154f6f4f49101f9b7b3954b716ebeb2c0337b
Caoshizhuo <64882259+Caoshizhuo@users.noreply.github.com>
2020-09-30 18:13:56 +0200

    reconfig_multi_topology_a

---
+++ reconfig_multi_topology_a/simulation/modelsim/rtl_work/reconfig_multi_topology_a/_primary.vhd
@@ -0,0 +1,13 @@
+library verilog;
+use verilog.vl_types.all;
+entity reconfig_multi_topology_a is
+    port(
+        a1              : in     vl_logic_vector(7 downto 0);
+        a2              : in     vl_logic_vector(7 downto 0);
+        a3              : in     vl_logic_vector(7 downto 0);
+        b               : in     vl_logic_vector(7 downto 0);
+        s0              : in     vl_logic;
+        s1              : in     vl_logic;
+        y               : out    vl_logic_vector(8 downto 0)
+    );
+end reconfig_multi_topology_a;
